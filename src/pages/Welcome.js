3a1089bf9efca662587b85c03c73f35b326217b6
davemo <david@wallner.bio>
2024-01-09 21:30:13 +0100

    need to change typed animation cause of version pb

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,31 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Typed from "react-typed";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Typewriter = ({ text, speed, onComplete }) => {
+  const [displayedText, setDisplayedText] = useState('');
+  const [index, setIndex] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDisplayedText((prevText) => prevText + text[index]);
+      setIndex((prevIndex) => prevIndex + 1);
+    }, speed);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [text, index, speed]);
+
+  useEffect(() => {
+    if (index === text.length) {
+      onComplete && onComplete();
+      navigate('/home');
+    }
+  }, [index, text, onComplete, navigate]);
+
+  return <span>{displayedText}</span>;
+};
 
 export default function Welcome() {
   const [showHomePage, setShowHomePage] = useState(false);
@@ -8,23 +33,16 @@ export default function Welcome() {
 
   const handleHomePage = () => {
     setShowHomePage(true);
-    navigate("/home");
+    navigate('/home');
   };
-
-
 
   return (
     <>
-    <div className="bg-black min-h-screen items-center flex flex-col">
-      <div className="text-4xl text-white flex flex-grow flex-col justify-center">
-        <Typed 
-        strings={["Welcome"]} 
-        typeSpeed={100} 
-        showCursor={true} 
-        onComplete={handleHomePage}
-        />
+      <div className="bg-black min-h-screen items-center flex flex-col">
+        <div className="text-4xl text-white flex flex-grow flex-col justify-center">
+          <Typewriter text="Welcome" speed={100} onComplete={handleHomePage} />
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
 }
